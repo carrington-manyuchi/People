2f4073cc21777c10925b10c60adf684dd3352ee9
Carrington Manyuchi <carrington.manyuchi@standardbank.co.za>
2025-03-21 09:33:45 +0200

    Setting up theme black and white colors

---
+++ People/Resources/Symbols.swift
@@ -0,0 +1,16 @@
+//
+//  Symbols.swift
+//  People
+//
+//  Created by Manyuchi, Carrington C on 2025/03/21.
+//
+
+import SwiftUI
+
+
+enum Symbols {
+    static let person = Image(systemName: "person.2")
+    static let gear = Image(systemName: "gears")
+    static let plus = Image(systemName: "plus")
+}
+
---
+++ People/Resources/Theme.swift
@@ -0,0 +1,15 @@
+//
+//  Theme.swift
+//  People
+//
+//  Created by Manyuchi, Carrington C on 2025/03/21.
+//
+
+import SwiftUI
+
+enum Theme {
+    static let background = Color("background")
+    static let detailBackground = Color("detail-background")
+    static let text = Color("text")
+    static let pill = Color("pill")
+}

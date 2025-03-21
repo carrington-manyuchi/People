6e80ea8b7a0c50f80fa28dcc7251c56d5858ece5
Carrington Manyuchi <carrington.manyuchi@standardbank.co.za>
2025-03-21 09:21:43 +0200

    Initial Commit

---
+++ People/PeopleApp.swift
@@ -0,0 +1,17 @@
+//
+//  PeopleApp.swift
+//  People
+//
+//  Created by Manyuchi, Carrington C on 2025/03/21.
+//
+
+import SwiftUI
+
+@main
+struct PeopleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
